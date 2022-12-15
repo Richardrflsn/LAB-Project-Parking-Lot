21af661b6a081ec9805e28815438855bd5e18f27
Richardrflsn <105922097+Richardrflsn@users.noreply.github.com>
2022-12-15 20:31:38 +0700

    Main Program

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int option;
+
+    do
+    {
+        printf("\nParking Lot RRCB\n");
+        printf("1. \n");
+        printf("2. \n");
+        printf("3. \n");
+        printf("Input options : ");
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 1:
+            
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        
+        default:
+            printf("No options!\n");
+            break;
+        }
+    } while (option != 3);
+    
+    return 0;
+}
